b8291a739163a2ecbb4ce828cc915d2daebc0b3d
karol <ongoras@gmail.com>
2026-01-01 15:36:18 +0100

    Init Spring Boot backend with WebSockets and Postgres

---
+++ backend/src/main/java/com/rpgmanager/backend/RpgmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rpgmanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgmanagerBackendApplication.class, args);
+	}
+
+}
