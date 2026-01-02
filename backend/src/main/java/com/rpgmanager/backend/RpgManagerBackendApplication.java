1a1f28f368e5f8cce53f7b5149fb5e2920df97d1
karol <ongoras@gmail.com>
2026-01-02 11:23:10 +0100

    Added Docker configuration

--- backend/src/main/java/com/rpgmanager/backend/RpgManagerBackendApplication.java
+++ backend/src/main/java/com/rpgmanager/backend/RpgManagerBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RpgmanagerBackendApplication {
+public class RpgManagerBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RpgmanagerBackendApplication.class, args);
+		SpringApplication.run(RpgManagerBackendApplication.class, args);
 	}
 
 }
